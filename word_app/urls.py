e41cb3652672e8c6368bf07abbc03df485f4ab54
Nikolat27 <samalizadeh899@gmail.com>
2024-07-03 13:11:08 +0330

    Features added: total characters and total words, Project finished

--- word_app/urls.py
+++ word_app/urls.py
@@ -1,6 +1,7 @@
 from . import views
 from django.urls import path
 
+app_name = "home_app"
 urlpatterns = [
-    path("")
+    path("", views.home_page, name="main")
 ]
--- word_app/views.py
+++ word_app/views.py
@@ -1,3 +1,19 @@
+import re
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+
+
+@csrf_exempt
+def home_page(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+        words = text.split()
+        total_characters = len(re.sub("a", "", text))
+        total_words = len(words)
+        return JsonResponse({"total_characters": total_characters, "total_words": total_words})
+    else:
+        return render(request, "word_app/index.html")
